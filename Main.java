dffd9799adc8305cc6f9fd55262470df262c6703
Lesli Bautista <lesliguadalupe34@gmail.com>
2025-12-04 19:20:48 -0600

    Estructura del programa

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class MetodoSimplex{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte opcion;
+        do{
+            System.out.println("\n \n \t \tMenu");
+            System.out.println("1. Maximizar");
+            System.out.println("2. Minimizar");
+            System.out.println("3. Salir");
+            System.out.println("Opcion");
+            opcion = sc.nextByte();
+            switch (opcion){
+                case 1-> {
+
+                }
+                case 2->{
+
+                }
+                case 3 ->{
+                    
+                }
+            }
+        }
+
+    }
+    
+}
